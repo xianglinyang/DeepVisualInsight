a78779c00752f5918e3f2f622f36f6abe1d53a12
hzf1174 <h1174538359@gmail.com>
2020-11-13 01:41:11 +0800

    Refactor code structure to support frontend

--- DeepView/demo_mlp_mnist.py
+++ DeepView/demo_mlp_mnist.py
@@ -19,69 +19,49 @@ def mnist(cuda=True, model_root=None):
         m = m.cuda()
     return m
 
-with open('DeepView\\mnist_data.json', 'r') as f:
-    dict = json.load(f)
+def demo_mlp_mnist(X, Y):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    torch_model = mnist(cuda=torch.cuda.is_available())
+    softmax = torch.nn.Softmax(dim=-1)
 
-X = []
-Y = []
+    # this is the prediction wrapper, it encapsulates the call to the model
+    # and does all the casting to the appropriate datatypes
+    def pred_wrapper(x):
+        with torch.no_grad():
+            x = np.array(x, dtype=np.float32)
+            tensor = torch.from_numpy(x).to(device)
+            logits = torch_model(tensor)
+            probabilities = softmax(logits).cpu().numpy()
+        return probabilities
 
-for data in dict["sampled_data"]:
-    vector = []
-    for dimension in data["vector"]:
-        vector.append(data["vector"][dimension])
-    X.append(vector)
-    Y.append(data["metadata"]["label"])
+    def visualization(image, point2d, pred, label=None, title=None):
+        f, a = plt.subplots()
+        a.set_title(title)
+        a.imshow(image.transpose([1, 2, 0]))
 
-X = X[:500]
-Y = Y[:500]
+    # the classes in the dataset to be used as labels in the plots
+    classes = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    # --- Deep View Parameters ----
+    batch_size = 512
+    max_samples = 5000
+    data_shape = (784,)
+    lam = 0  # default parameter
+    title = 'MLP - MNIST'
 
-X = np.array(X)
-Y = np.array(Y)
+    umapParms = {
+        "random_state": 42 * 42,
+        "n_neighbors": 30,
+        "spread": 1,
+        "min_dist": 0.1,
+        "a": 600,
+        "metric": "euclidean"
+    }
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-torch_model = mnist(cuda=torch.cuda.is_available())
-softmax = torch.nn.Softmax(dim=-1)
+    deepview = DeepView(pred_wrapper, classes, max_samples, batch_size,
+                        data_shape, lam=lam, title=title, metric="euclidean", clip_certainty=0.25)
+    deepview._init_mappers(None, None, umapParms)
 
-
-# this is the prediction wrapper, it encapsulates the call to the model
-# and does all the casting to the appropriate datatypes
-def pred_wrapper(x):
-    with torch.no_grad():
-        x = np.array(x, dtype=np.float32)
-        tensor = torch.from_numpy(x).to(device)
-        logits = torch_model(tensor)
-        probabilities = softmax(logits).cpu().numpy()
-    return probabilities
-
-
-def visualization(image, point2d, pred, label=None, title=None):
-    f, a = plt.subplots()
-    a.set_title(title)
-    a.imshow(image.transpose([1, 2, 0]))
-
-
-# the classes in the dataset to be used as labels in the plots
-classes = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-# --- Deep View Parameters ----
-batch_size = 512
-max_samples = 5000
-data_shape = (784,)
-lam = 0  # default parameter
-title = 'MLP - MNIST'
-
-umapParms = {
-    "random_state": 42 * 42,
-    "n_neighbors": 30,
-    "spread": 1,
-    "min_dist": 0.1,
-    "a": 600,
-    "metric": "euclidean"
-}
-
-deepview = DeepView(pred_wrapper, classes, max_samples, batch_size,
-                    data_shape, lam=lam, title=title, metric="euclidean", clip_certainty=0.25)
-deepview._init_mappers(None, None, umapParms)
-
-deepview.add_samples(X, Y)
-deepview.show()
-
+    deepview.add_samples(X, Y)
+    result = deepview.show_tensorboard()
+    deepview.close()
+    return result
