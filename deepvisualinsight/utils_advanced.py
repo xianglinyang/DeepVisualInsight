356ef3fab8bc6f15768876c5f893356fa1ba7d0e
lindsey98 <liny@xgpd5.comp.nus.edu.sg>
2021-03-10 22:26:17 +0800

    Modify clustering, Add C&W L2 adv attack, Add new constraint for GAP layer direction

--- deepvisualinsight/utils_advanced.py
+++ deepvisualinsight/utils_advanced.py
@@ -7,122 +7,214 @@ import json
 import os
 import sys
 import math
+from tqdm import tqdm
+from torch import optim
+
+####### Those packages are for my own convenience can be removed #######
+from model import resnet18
+########################################################################
 
 def clustering(data, predictions, n_clusters_per_cls, n_class=10, verbose=0):
     """
     clustering function
-    :param data: [n_samples, n_features]
-    :param predictions: predictions of shape [n_samples]
-    :param n_clusters_per_cls: int, how many clusters that user want for each class
-    :param verbose, by default 0
-    :return: centers, shape [n_class, n_clusters_per_cls, n_features]
+    :param data: GAP numpy.ndarray of shape (N, 512)
+    :param predictions: class prediction numpy.ndarray of shape (N,)
+    :param n_clusters_per_cls: number of clusters for each class
+    :param n_class: number of classes, e.g. 10 for CIFAR10
+    :param verbose: enable message printing
+    :return kmeans_result: predicted cluster label numpy.ndarray of shape (N,)
+    :return predictions: same as input
     """
-    centers = []
+    kmeans_result = np.zeros_like(predictions) # save kmeans results
     
     t0 = time.time()
     for i in tqdm(range(n_class)):
         c = data[np.argwhere(predictions == i).squeeze()]
         if len(c) == 0: # no data is predicted as label i
-            centers.append([])
+            continue
         else:
-            kmeans = KMeans(n_clusters=n_clusters_per_cls, random_state=0).fit(c) # perform Kmeans clustering
-            centers.append(kmeans.cluster_centers_)
+            # perform Kmeans clustering
+            kmeans = KMeans(n_clusters=n_clusters_per_cls, random_state=0).fit(c) 
+            kmeans_result[np.argwhere(predictions == i).squeeze()] = kmeans.labels_ # get cluster labels
+            
     t1 = time.time()
     
-    centers = np.asarray(centers)
-#     print(centers.shape) #TODO: comment this 
-    
     if verbose > 0:
-        print("Clustering {:d} classes in {:.2f} seconds...".format(len(centers), t1 - t0))
-    return centers
+        print("Clustering {:d} classes in {:.2f} seconds...".format(n_class, t1 - t0))
+        
+    return kmeans_result, predictions
 
-def cal_loss(output, target, dist, scale_const):
-    '''Helper function Compute loss for C&W L2'''
-    # compute the probability of the label class versus the maximum other
-    real = target
-    other = ((1. - target) * output - target * 10000.).max(1)[0]
-    # if targeted, optimize for making the other class most likely
-    loss1 = torch.clamp(other - real + confidence, min=0.)  # equiv to max(..., 0.)
-    loss1 = torch.sum(scale_const * loss1)
+def cw_l2_attack(model, image, label, target_cls, target_gap,
+                 c=1e-2, kappa=0, max_iter=1000, learning_rate=0.01, verbose=1) :
+    '''
+    Implementation of C&W L2 targeted attack, Modified from https://github.com/Harry24k/CW-pytorch
+    :param model: subject model
+    :param image: image to attack torch.Tensor of shape (1, C, H, W)
+    :param label: original predicted class int value 
+    :param target_cls: target class int value 
+    :param target_gap: targeted gap layer torch.Tensor of shape (1, 512)
+    :param c: trade-off parameter in loss float
+    :param kappa: margin in confidence loss float
+    :param max_iter: maximum number of attack iter int
+    :param learning_rate: learning rate for optimizer float
+    :verbose: enable printing
+    :return attack_image: perturbed image torch.Tensor of shape (1, C, H, W)
+    :return successful: indicating whether the attack is successful or not boolean value
+    '''
+    
+    # Define f-function
+    def f(x) :
+        
+        output = model(x)
+        one_hot_label = torch.eye(len(output[0]))[label].to(device)
+        one_hot_target = torch.eye(len(output[0]))[target_cls].to(device)
 
-    loss2 = dist.sum()
+        # confidence for the original predicted class and target class
+        i, j = torch.masked_select(output, one_hot_label.bool()), torch.masked_select(output, one_hot_target.bool())
+        
+        # optimize for making the other class most likely 
+        return torch.clamp(i-j, min=-kappa)
+    
+    successful = False
+    
+    # initialize w : the noise
+    w = torch.zeros_like(image, requires_grad=True).to(device)
+    optimizer = optim.Adam([w], lr=learning_rate) # an optimizer specifically for w
 
-    loss = loss1 + loss2
-    return loss
+    for step in range(max_iter) :
+        # w is the noise added to the original image, restricted to be [-1, 1]
+        a = image + torch.tanh(w) 
 
+        loss1 = nn.MSELoss(reduction='sum')(a, image) # L2 norm between original image and perturbed image
+        loss2 = torch.sum(c*f(a)) # confidence_diff between original and target class
+        
+        # Add a third loss to minimize the distance between gap layers
+        gap_a = gap_model(model)(a)
+        gap_a = gap_a.view((gap_a.shape[0], gap_a.shape[1]))
+        loss3 = nn.MSELoss(reduction='sum')(gap_a, target_gap) 
 
-def adv_attack(image, epsilon, data_grad):
-    """fgsm adversarial attack"""
-    sign_data_grad = torch.sign(data_grad)
-    perturbed_image = image + epsilon * sign_data_grad
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    return perturbed_image
-
-
-def get_border_points(data_, target_, model, diff, device, epsilon=.01, limit=5,):
-    """get border points by fgsm adversarial attack?"""
-
-    num = len(data_)
-    model.eval()
-
-    # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    criterion = nn.CrossEntropyLoss()
-
-    adv_data = np.zeros(data_.shape) 
-    adv_pred_labels = np.zeros(target_.shape)
-    r = 0
-
-    for i in range(num): # generate adversarial for each CP
-        data, target = data_[i:i+1], target_[i:i+1]
-        data = data.to(device=device, dtype=torch.float)
-        target = target.to(device=device, dtype=torch.long)
-
-        data.requires_grad = True
-        j = 1
-        while True:
-            output = model(data)
-
-            loss = criterion(output, target)  # loss for ground-truth class
-            model.zero_grad()
-            loss.backward()
-            data_grad = data.grad.data
-
-            perturbed_data = adv_attack(data, epsilon, data_grad)
-
-            output = model(perturbed_data)
-            sort, _ = torch.sort(output, dim=-1, descending=True)
-            abs_dis = sort[0, 0] - sort[0, 1]
-
-            final_pred = output.max(1, keepdim=True)[1]
-
-            adv_ex = perturbed_data.squeeze(0).detach().cpu().numpy()
-            data = torch.from_numpy(np.expand_dims(adv_ex, axis=0)).to(device)
-            data.requires_grad = True
-            j = j + 1
+        cost = loss1 + loss2 + loss3
+        
+        # Backprop: jointly optimize the loss
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+        
+        # New prediction
+        with torch.no_grad():
+            pred_new = model(a)
+        
+        # Stop when ... 
+        # successfully flip the label
+        if torch.argmax(pred_new, dim=1).item() == target_cls: 
+            successful = True
+            break
             
-            # Stop when ... 
-            # successfully flip the label
-            if final_pred.item() != target: 
-                if abs_dis < diff:
-                    adv_data[r] = adv_ex
-                    adv_pred_labels[r] = final_pred.item()
-                    r = r + 1
-                break
-            # reach the boundary
-            if abs_dis < diff: 
-                adv_data[r] = adv_ex
-                adv_pred_labels[r] = final_pred.item()
-                r = r + 1
-                break
-             # number of iterations reach maximum iteration allowed
-            if j > limit:
-                break
-                
-    adv_data = adv_data[:r]
-    # adv_pred_labels = adv_pred_labels[:r]
-    return adv_data
+        if verbose > 0:
+            print('- Learning Progress : %2.2f %% ' %((step+1)/max_iter*100), end='\r')
+            
+    # w is the noise added to the original image, restricted to be [-1, 1]
+    attack_images = image + torch.tanh(w) 
+
+    return attack_images.detach().cpu(), successful
+
+def get_border_points(model, input_x, gaps, kmeans_result, predictions, 
+                      num_adv_eg = 5000, num_cls = 10, n_clusters_per_cls = 10, verbose=1):
+    '''Get BPs
+    :param model: subject model
+    :param input_x: images torch.Tensor of shape (N, C, H, W)
+    :param gaps: GAP torch.Tensor of shape (N, 512)
+    :param kmeans_result: predicted cluster label numpy.ndarray of shape (N,)
+    :param predictions: class prediction numpy.ndarray of shape (N,)
+    :param num_adv_eg: number of adversarial examples to be generated
+    :param num_cls: number of classes, eg 10 for CIFAR10
+    :param n_clusters_per_cls: number of clusters for each class
+    :return adv_examples: adversarial images torch.Tensor of shape (N, C, H, W)
+    '''
+
+    ct = 0
+    adv_examples = torch.tensor([])
+
+    t0 = time.time()
+    while ct < num_adv_eg:
+
+        # randomly select two classes
+        cls1 = np.random.choice(range(num_cls), 1)[0]
+        cls2 = cls1
+        while cls2 == cls1: # choose a different class
+            cls2 = np.random.choice(range(num_cls), 1)[0]
+
+        # randomly select one cluster from each class
+        cluster1 = np.random.choice(range(n_clusters_per_cls), 1)[0]
+        while np.sum((predictions == cls1) & (kmeans_result == cluster1)) <= 1: # avoid empty cluster
+            cluster1 = np.random.choice(range(n_clusters_per_cls), 1)[0]
+
+        cluster2 = np.random.choice(range(n_clusters_per_cls), 1)[0]
+        while np.sum((predictions == cls2) & (kmeans_result == cluster2)) <= 1: # avoid empty cluster
+            cluster2 = np.random.choice(range(n_clusters_per_cls), 1)[0]
+
+        # randomly select one image for each cluster
+        data1_index = np.argwhere((predictions == cls1) & (kmeans_result == cluster1)).squeeze()
+        data2_index = np.argwhere((predictions == cls2) & (kmeans_result == cluster2)).squeeze()
+        image1_idx = np.random.choice(range(len(data1_index)), size=1) 
+        image2_idx = np.random.choice(range(len(data2_index)), size=1) 
+
+        image1 = input_x[data1_index[image1_idx]]
+        image2 = input_x[data2_index[image2_idx]]
+
+        gap1 = gaps[data1_index[image1_idx]]
+        gap2 = gaps[data2_index[image2_idx]]
+
+        # attack from cluster 1 to cluster 2
+        attack1, successful1 = cw_l2_attack(model, image1.to(device), cls1, cls2, gap2)
+
+        # attack from cluster 2 to cluster 1
+        attack2, successful2 = cw_l2_attack(model, image2.to(device), cls2, cls1, gap1)
+
+        if successful1:
+            adv_examples = torch.cat((adv_examples, attack1), dim=0)
+            ct += 1
+        if successful2:
+            adv_examples = torch.cat((adv_examples, attack2), dim=0)
+            ct += 1
+        
+        if verbose:
+            if ct % 1000 == 0:
+                print('{}/{}'.format(ct, num_adv_eg))
+
+    t1 = time.time()
+    if verbose:
+        print('Total time {:2f}'.format(t1-t0))
+        
+    return adv_examples
 
 
+def batch_run(model, data, output_shape, batch_size=200):
+    '''Get GAP layers and predicted labels for data'''
+    gaps = torch.tensor([])
+    preds = torch.tensor([])
+    
+    n_batches = max(math.ceil(len(data) / batch_size), 1)
+    for b in range(n_batches):
+        r1, r2 = b * batch_size, (b + 1) * batch_size
+        inputs = data[r1:r2]
+        inputs = inputs.to(device, dtype=torch.float)
+        
+        with torch.no_grad():
+            pred = torch.argmax(model(inputs), dim=1)
+            gap = gap_model(model)(inputs)
+            gap = gap.view((gap.shape[0], gap.shape[1]))
+            
+            gaps = torch.cat((gaps, gap.detach().cpu()), dim=0)
+            preds = torch.cat((preds, pred.detach().cpu().float()), dim=0)
+         
+    return gaps.numpy(), preds.numpy()
+
+
+
+
+
+###################################### I didn't change those functions ######################################
 def load_labelled_data_index(filename):
     if not os.path.exists(filename):
         sys.exit("data file doesn't exist!")
@@ -138,16 +230,30 @@ def softmax_model(model): # softmax layer
 def gap_model(model): # GAP layer
     return torch.nn.Sequential(*(list(model.children())[:-1]))
 
+##################################################################################################################
 
-def batch_run(model, data, output_shape, batch_size=200):
-    data = data.to(dtype=torch.float)
-    input_X = np.zeros([len(data), output_shape])
-    n_batches = max(math.ceil(len(data) / batch_size), 1)
-    for b in range(n_batches):
-        r1, r2 = b * batch_size, (b + 1) * batch_size
-        inputs = data[r1:r2]
-        with torch.no_grad():
-            pred = model(inputs).cpu().numpy()
-            input_X[r1:r2] = pred.squeeze()
-    return input_X
 
+if __name__ == '__main__':
+    
+    ## Load model
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model = resnet18()
+    checkpoint = torch.load('subject_model.pth')
+    model.load_state_dict(checkpoint)
+    model.to(device)
+    
+    # Load data
+    input_x = torch.load('Training_data/training_dataset_data.pth')
+    y = torch.load('Training_data/training_dataset_label.pth')
+    
+    # Get GAP and predicted labels
+    gaps, preds = batch_run(model, input_x, output_shape, batch_size=200)
+    
+    # Kmeans clustering
+    kmeans_result, predictions = clustering(gaps, preds, n_clusters_per_cls=10)
+    
+    # Adversarial attacks
+    adv_examples = get_border_points(model, input_x, gaps, kmeans_result, predictions, 
+                                     num_adv_eg = 5000, num_cls = 10, n_clusters_per_cls = 10, verbose=1)
+    
+    
