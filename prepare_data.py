98208ee8db773178726a3e25e56a696d9feb76fe
hzf1174 <h1174538359@gmail.com>
2021-03-20 22:30:32 +0800

    Prepare data

--- prepare_data.py
+++ prepare_data.py
@@ -4,24 +4,57 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import json
 
 def prepare_data(content_path, data, iteration, resolution, folder_name, direct_call=True):
     sys.path.append(content_path)
     from Model.model import ResNet18
+    #from Model.model import resnet18
     prefix = folder_name + '/'
     if direct_call:
         prefix = 'server/'+folder_name + '/'
-    # net = resnet18()
+    #net = resnet18()
     net = ResNet18()
   
 
     classes = ("airplane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 
-    mms = MMS(content_path, net, 1, 200, 512, 10, classes, cmap="tab10", resolution=resolution, boundary_diff=1, neurons=128,
+    mms = MMS(content_path, net, 1, 6, 1, 512, 10, classes, cmap="tab10", resolution=resolution, neurons=256,
               verbose=1)
     # mms.data_preprocessing()
     # mms.prepare_visualization_for_all()
-
+    evaluation = {}
+    evaluation['nn_train_10'] = mms.proj_nn_perseverance_knn_train(iteration, 10)
+    evaluation['nn_train_15'] = mms.proj_nn_perseverance_knn_train(iteration, 15)
+    evaluation['nn_train_30'] = mms.proj_nn_perseverance_knn_train(iteration, 30)
+    evaluation['nn_test_10'] = mms.proj_nn_perseverance_knn_test(iteration, 10)
+    evaluation['nn_test_15'] = mms.proj_nn_perseverance_knn_test(iteration, 15)
+    evaluation['nn_test_30'] = mms.proj_nn_perseverance_knn_test(iteration, 30)
+    evaluation['bound_train_10'] = mms.proj_boundary_perseverance_knn_train(iteration, 10)
+    evaluation['bound_train_15'] = mms.proj_boundary_perseverance_knn_train(iteration, 15)
+    evaluation['bound_train_30'] = mms.proj_boundary_perseverance_knn_train(iteration, 30)
+    evaluation['bound_test_10'] = mms.proj_boundary_perseverance_knn_test(iteration, 10)
+    evaluation['bound_test_15'] = mms.proj_boundary_perseverance_knn_test(iteration, 15)
+    evaluation['bound_test_30'] = mms.proj_boundary_perseverance_knn_test(iteration, 30)
+    print("finish half of it")
+    evaluation['inv_nn_train_10'] = mms.inv_nn_preserve_train(iteration, 10)
+    evaluation['inv_nn_train_15'] = mms.inv_nn_preserve_train(iteration, 15)
+    evaluation['inv_nn_train_30'] = mms.inv_nn_preserve_train(iteration, 30)
+    evaluation['inv_nn_test_10'] = mms.inv_nn_preserve_test(iteration, 10)
+    evaluation['inv_nn_test_15'] = mms.inv_nn_preserve_test(iteration, 15)
+    evaluation['inv_nn_test_30'] = mms.inv_nn_preserve_test(iteration, 30)
+    evaluation['inv_acc_train'] = mms.inv_accu_train(iteration)
+    evaluation['inv_acc_test'] = mms.inv_accu_test(iteration)
+    evaluation['inv_conf_train'] = mms.inv_conf_diff_train(iteration)
+    evaluation['inv_conf_test'] = mms.inv_conf_diff_test(iteration)
+    with open(prefix+'evaluation_'+str(iteration)+'.json', 'w') as f:
+        json.dump(evaluation, f)
+        
+    gap_layer_data = mms.get_representation_data(iteration, data)
+    prediction = mms.get_pred(iteration, gap_layer_data).argmax(-1)
+    with open(prefix+'prediction_'+str(iteration)+'.npy', 'wb') as f:
+        np.save(f, prediction)
+        
     dimension_reduction_result = mms.batch_get_embedding(data, iteration)
 
     with open(prefix+'dimension_reduction_'+str(iteration)+'.npy', 'wb') as f:
@@ -40,9 +73,13 @@ def prepare_data(content_path, data, iteration, resolution, folder_name, direct_
 
     with open(prefix+'color.npy', 'wb') as f:
         np.save(f, color)
-
+        
 if __name__ == "__main__":
-    content_path = "E:\\DVI_exp_data\\active_learning\\entropy_tl_cifar10"
-    testing_data = torch.load(
-        "E:\\DVI_exp_data\\active_learning\\entropy_tl_cifar10\\Testing_data\\testing_dataset_data.pth")
-    prepare_data(content_path, testing_data, iteration=180, folder_name="data/entropy_tl_cifar10", resolution=400)
+    content_path = "/home/selab/Enviroment/data/new_random_tl_cifar10"
+    training_data = torch.load("/home/selab/Enviroment/data/resnet18_cifar10/data/training_dataset_data.pth")
+    testing_data = torch.load("/home/selab/Enviroment/data/resnet18_cifar10/data/testing_dataset_data.pth")
+    data = torch.cat((training_data, testing_data), 0)
+    print("start")
+    for i in range(1, 7):
+      print("prepare for iteration: " + str(i))
+      prepare_data(content_path, data, iteration=i, folder_name="data/new_random_tl_cifar10", resolution=200)
