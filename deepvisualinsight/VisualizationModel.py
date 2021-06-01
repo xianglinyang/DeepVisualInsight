e0538d154e18e63deaab3f214f2f9cad371ad6e9
xianglin <yxlyzxl@gmail.com>
2021-06-01 22:49:28 +0800

    refactor

--- deepvisualinsight/VisualizationModel.py
+++ deepvisualinsight/VisualizationModel.py
@@ -59,8 +59,8 @@ class ParametricModel(keras.Model):
                 prev_trainable_variables = self.prev_trainable_variables
 
                 # embedding loss
-                embed_loss_to = self.loss["embedding_to"](_, embedding_to)
-                embed_loss_to_recon = self.loss["embedding_to_recon"](_, embedding_to_recon)
+                # embed_loss_to = self.loss["embedding_to"](_, embedding_to)
+                # embed_loss_to_recon = self.loss["embedding_to_recon"](_, embedding_to_recon)
                 #
                 # final_grad_result_list = list()
                 #
