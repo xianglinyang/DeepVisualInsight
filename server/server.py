36b9bd096abafce8b612ae6178fb7468c9235069
xianglin <yxlyzxl@gmail.com>
2021-06-09 12:54:26 +0800

    refactor

--- server/server.py
+++ server/server.py
@@ -1,12 +1,11 @@
 from flask import request, Flask, jsonify, make_response
 from flask_cors import CORS, cross_origin
 
-import os, sys
+import os
+import sys
 import numpy as np
-import base64
 import json
 import torch
-import math
 
 lib_path = os.path.abspath(os.path.join('..'))
 sys.path.append(lib_path)
@@ -23,21 +22,21 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def animation():
     res = request.get_json()
     path = res['path']
-    data_path = res['data_path']
+    # data_path = res['data_path']
     iteration = res['iteration']
     cache = res['cache']
     resolution = int(res['resolution'])
-    folder_path = 'data/' + path.split('/')[-1]
+    folder_path = os.path.join('data', path.split('/')[-1])
     if not os.path.isdir(folder_path):
         os.mkdir(folder_path)
     
-    path_files = os.listdir(path+'/Model')
+    path_files = os.listdir(os.path.join(path+'Model'))
     maximum_iteration = len(path_files) - 2
     
-    training_data = torch.load(data_path + "/training_dataset_data.pth")
-    training_labels = torch.load(data_path + "/training_dataset_label.pth")
-    testing_data = torch.load(data_path + "/testing_dataset_data.pth")
-    testing_labels = torch.load(data_path + "/testing_dataset_label.pth")
+    training_data = torch.load(os.path.join(path, "Training_data", "training_dataset_data.pth"))
+    training_labels = torch.load(os.path.join(path, "Training_data", "training_dataset_label.pth"))
+    testing_data = torch.load(os.path.join(path, "Testing_data", "testing_dataset_data.pth"))
+    testing_labels = torch.load(os.path.join(path, "Testing_data", "testing_dataset_label.pth"))
     
     training_data_number = training_data.shape[0]
     testing_data_number = testing_data.shape[0]
@@ -49,23 +48,23 @@ def animation():
     if not res['cache']:
         prepare_data(path, data, iteration, resolution, folder_path, False)
 
-    with open(folder_path+'/dimension_reduction_'+str(iteration)+'.npy', 'rb') as f:
+    with open(os.path.join(folder_path, 'dimension_reduction_'+str(iteration)+'.npy'), 'rb') as f:
         result = np.load(f).tolist()
 
-    with open(folder_path+'/grid_'+str(iteration)+'.npy', 'rb') as f:
+    with open(os.path.join(folder_path, 'grid_'+str(iteration)+'.npy'), 'rb') as f:
         grid_index = np.load(f)
         grid_index = grid_index.reshape((-1, 2)).tolist()
-    with open(folder_path+'/decision_view_'+str(iteration)+'.npy', 'rb') as f:
+    with open(os.path.join(folder_path, 'decision_view_'+str(iteration)+'.npy'), 'rb') as f:
         grid_color = np.load(f)
         grid_color = grid_color.reshape((-1, 3))
         grid_color *= 255
         grid_color = grid_color.astype(int).tolist()
 
-    with open(folder_path+'/color.npy', 'rb') as f:
+    with open(os.path.join(folder_path, 'color.npy'), 'rb') as f:
         standard_color = np.load(f)*255
         standard_color = standard_color.astype(int).tolist()
     
-    with open(folder_path+'/evaluation_'+str(iteration)+'.json', 'r') as f:
+    with open(os.path.join(folder_path, 'evaluation_'+str(iteration)+'.json'), 'r') as f:
         evaluation = json.load(f)
         evaluation_new = evaluation
         for item in evaluation:
@@ -81,34 +80,33 @@ def animation():
         label_list.append(classes[label])
     
     prediction_list = []
-    with open(folder_path+'/prediction_'+str(iteration)+'.npy', 'rb') as f:
+    with open(os.path.join(folder_path, 'prediction_'+str(iteration)+'.npy'), 'rb') as f:
          prediction = np.load(f).tolist()
          for pred in prediction:
              prediction_list.append(classes[pred])
     
-    current_training_path = folder_path+'/current_training_'+str(iteration)+'.json'
+    current_training_path = os.path.join(folder_path, 'current_training_'+str(iteration)+'.json')
     if os.path.isfile(current_training_path):
          with open(current_training_path, 'r') as f:
               current_training = json.load(f)
     else:
          current_training = training_data_index
-         
-         
-    new_selection_path = folder_path+'/new_selection_'+str(iteration)+'.json'
+
+    new_selection_path = os.path.join(folder_path, 'new_selection_'+str(iteration)+'.json')
     if os.path.isfile(new_selection_path):
          with open(new_selection_path, 'r') as f:
               new_selection = json.load(f)
     else:
          new_selection = []
          
-    noisy_data_path = folder_path+'/noisy_data_index.json'
+    noisy_data_path = os.path.join(folder_path, 'noisy_data_index.json')
     if os.path.isfile(noisy_data_path):
          with open(noisy_data_path, 'r') as f:
               noisy_data = json.load(f)
     else:
          noisy_data = []
     
-    original_label_path = folder_path+'/original_label.npy'
+    original_label_path = os.path.join(folder_path, 'original_label.npy')
     original_label_list = []
     if os.path.isfile(original_label_path):
          with open(original_label_path, 'rb') as f:
@@ -118,19 +116,19 @@ def animation():
     else:
          original_label_list = label_list
 
-    with open(folder_path + '/acc' + str(iteration) + '.json', 'r') as f:
+    with open(os.path.join(folder_path, 'acc' + str(iteration) + '.json'), 'r') as f:
         acc = json.load(f)
         acc_train = round(acc['training'], 2)
         acc_test = round(acc['testing'], 2)
         evaluation_new['acc_train'] = acc_train
         evaluation_new['acc_test'] = acc_test
 
-    with open(folder_path + '/inv_acc_' + str(iteration) + '.npy', 'rb') as f:
+    with open(os.path.join(folder_path, 'inv_acc_' + str(iteration) + '.npy'), 'rb') as f:
         inv_acc_list = np.load(f).tolist()
 
-    uncertainty_path = folder_path + '/uncertainty_' + str(iteration) + '.json'
-    diversity_path = folder_path + '/diversity_' + str(iteration) + '.json'
-    tot_path = folder_path + '/tot_' + str(iteration) + '.json'
+    uncertainty_path = os.path.join(folder_path, 'uncertainty_' + str(iteration) + '.json')
+    diversity_path = os.path.join(folder_path, 'diversity_' + str(iteration) + '.json')
+    tot_path = os.path.join(folder_path, 'tot_' + str(iteration) + '.json')
     is_uncertainty_diversity_tot_exist = True
     uncertainty_diversity_tot_dict = {}
     if os.path.isfile(uncertainty_path):
