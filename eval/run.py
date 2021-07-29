db504201a47d0ea08bc5b92cfb6870bacef011bd
xianglin <yxlyzxl@gmail.com>
2021-07-29 17:27:44 +0800

    batch run experiments

--- eval/run.py
+++ eval/run.py
@@ -18,6 +18,8 @@ def get_arguments():
     parser.add_argument("--cuda", type=bool, default=False)
     parser.add_argument("--num_classes", type=int, default=10)
     parser.add_argument("--split", type=int, default=-1)
+    parser.add_argument("-t", type=float)
+    parser.add_argument("-a", type=float)
 
     return parser.parse_args()
 
@@ -35,6 +37,8 @@ if __name__ == "__main__":
     dataset = args.dataset
     cuda = args.cuda
     split = args.split
+    alpha = args.a
+    temperature = args.t
     if cuda:
         attack_device = "cuda:0"
     else:
@@ -60,9 +64,10 @@ if __name__ == "__main__":
     else:
         classes = ("T-shirt", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot")
 
-    mms = MMS(content_path, net, epoch_start, epoch_end, epoch_period, embedding_dim, num_classes, classes,
+    mms = MMS(content_path, net, epoch_start, epoch_end, epoch_period, embedding_dim, num_classes, classes, temperature=temperature,
               cmap="tab10",resolution=resolution, neurons=neurons, verbose=1, temporal=False, split=split,
-              advance_border_gen=True, attack_device=attack_device)
+              advance_border_gen=True, alpha=alpha, withoutB=False, attack_device=attack_device)
+
     # encoder_location = os.path.join(content_path, "Model", "Epoch_{:d}".format(136), "encoder_advance")
     # encoder = tf.keras.models.load_model(encoder_location)
     # decoder_location = os.path.join(content_path, "Model", "Epoch_{:d}".format(136), "decoder_advance")
@@ -70,4 +75,4 @@ if __name__ == "__main__":
 
     mms.data_preprocessing()
     mms.prepare_visualization_for_all()
-    mms.save_evaluation()
+    mms.save_evaluation(eval=True)
